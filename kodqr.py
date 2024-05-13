58eadfa8db18a93436fdcd1754087b9b9ca42e9b
robotnik <112630991+robotnikhub@users.noreply.github.com>
2024-05-13 18:31:54 +0200

    Update and rename qrcode.py to kodqr.py

--- kodqr.py
+++ kodqr.py
@@ -1,6 +1,7 @@
 import customtkinter
 from PIL import Image
 import qrcode
+from tkinter import messagebox
 
 def button_callback():
     print("button pressed")
@@ -8,6 +9,7 @@ def button_callback():
 def hello():
     text = textbox.get("0.0", "end").strip()
     if len(text) == 0:
+        messagebox.showwarning(title="QR CODE ERROR [0X01]", message="No data was provided")
         raise Exception('No data was provided')
         
     print(text)
@@ -22,12 +24,12 @@ def hello():
 
     img = qr.make_image(fill_color="black", back_color="white")
     img.save("test.png")
-    my_image = customtkinter.CTkImage(light_image=Image.open("C:\\Users\\stahu\\Desktop\\oooo\\qr-code\\test.png"),
-                                  dark_image=Image.open("C:\\Users\\stahu\\Desktop\\oooo\\qr-code\\test.png"),
+    my_image = customtkinter.CTkImage(light_image=Image.open(".\\test.png"),
+                                  dark_image=Image.open(".\\test.png"),
                                   size=(100, 100))
     image_label = customtkinter.CTkLabel(master=app, image=my_image, text="")  # display image with a CTkLabel 
-    image_label.pack(pady='10')
-
+    image_label.place(x=70, y=100)
+    print(my_image)
 
 
 
@@ -39,15 +41,13 @@ column_y = int(resolution_y/2)
 app = customtkinter.CTk()
 app.title("generowanie kodu QR")
 app.geometry(f'{resolution_x}x{resolution_y}')
-
-# app.grid_columnconfigure(0, weight=2)
+app.resizable(False, False)
 
 textbox = customtkinter.CTkTextbox(master=app, width=resolution_x, height=50)
 textbox.pack(anchor='s')
 
 button = customtkinter.CTkButton(master=app, text="Generuj kod QR", command=hello)
 button.pack(anchor='n')
-
 
 
 app.mainloop()
